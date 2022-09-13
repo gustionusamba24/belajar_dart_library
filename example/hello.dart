1e431683945eefe57472e7fc7b44f7919fedc780
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2022-09-13 12:46:48 +0700

    Dart Library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(List<String> args) {
+  print(sayHello("Gustio Nusamba"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
